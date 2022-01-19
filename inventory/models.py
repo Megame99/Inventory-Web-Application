27475f1d176cce52a8a39173626c9fd9aa937eb2
Megame99 <83548747+Megame99@users.noreply.github.com>
2022-01-19 13:07:20 -0500

    Update models.py

--- inventory/models.py
+++ inventory/models.py
@@ -1,23 +1,22 @@
 
-
 # Create your models here.
-from django.db.models import CheckConstraint, Q, constraints
+
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+
 CATEGORY_CHOICES = (
-    ("1","Electronics"),
-    ("2", "Appliances"),
-    ("3","Home Decor"),
-    ("4","Misc"),
-    ("5","Clothing"),
-    ("6","Footwear"),
-    ("7","Sports"),
-    ("8","Health and Beauty"),
+    ("Electronics","Electronics"),
+    ("Appliances", "Appliances"),
+    ("Home Decor","Home Decor"),
+    ("Misc","Misc"),
+    ("Clothing","Clothing"),
+    ("Footwear","Footwear"),
+    ("Sports","Sports"),
+    ("Health and Beauty","Health and Beauty"),
 )
 
 class Item(models.Model):
     name = models.CharField(max_length=50)
-    category = models.CharField(max_length=20, choices = CATEGORY_CHOICES, default='4')
+    category = models.CharField(max_length=20, choices = CATEGORY_CHOICES, default='6')
     quantity = models.PositiveIntegerField()
     description = models.TextField(max_length=250)
     itemcode = models.PositiveIntegerField(unique=True)
